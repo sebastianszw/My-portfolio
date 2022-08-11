911e8c753f8c25a8c2ddad35d3e7412dd812e0bd
Sebastian <sebastianszw@gmail.com>
2022-08-11 03:36:14 +0200

    Header, Projects section, Footer
    Added/Changed :
    -changed header background
    -added projects section
    -added footer
    -added animations for projects section and footer
    -added files for @media and css animations
    -added img files
    -change container padding
    -added animation for background in nav
    -and many many more!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+/*Navbar background scroll*/
+const nav = document.querySelector('nav');
+window.addEventListener('scroll', function () {
+    const offset = window.pageYOffset;
+    if (offset > 10)
+        nav.classList.add('scroll')
+    else
+        nav.classList.remove('scroll')
+});
+
+/*freeze scroll*/
 const timeout = setTimeout(enableScroll, 2000)
 function enableScroll() {
     const scroll = document.querySelector("body");
@@ -6,11 +17,11 @@ function enableScroll() {
 
 /*Nav logo*/
 gsap.fromTo(".logo",
-    { top: 500, height: 100 }, { top: 0, height: 40, duration: 1 }
+    { top: '50vh', height: 100 }, { top: 0, height: 40, duration: 1 }
 ).delay(1);
 
 /*Main content*/
-gsap.fromTo(".content",
+gsap.fromTo("header",
     { opacity: 0 }, { opacity: 1 }
 ).delay(1.1);
 
@@ -22,3 +33,29 @@ gsap.fromTo(".headerText",
 gsap.fromTo(".headerIcon",
     { opacity: 0 }, { opacity: 1 }
 ).delay(1.6);
+
+/*Projects container*/
+gsap.fromTo(".portfolioContainer", { opacity: 0, y: 100 }, {
+    scrollTrigger: ".portfolioContainer",
+    opacity: 1,
+    y: 0,
+    delay: 1,
+    ease: Power0.easeNone
+});
+
+gsap.fromTo(".projectsContainer", { opacity: 0, y: 100 }, {
+    scrollTrigger: ".portfolioContainer",
+    opacity: 1,
+    y: 0,
+    delay: 1.4,
+    ease: Power0.easeNone
+});
+
+/*Footer*/
+gsap.fromTo("footer", { opacity: 0, y: 100 }, {
+    scrollTrigger: "footer",
+    opacity: 1,
+    y: 0,
+    delay: 1,
+    ease: Power0.easeNone
+});
